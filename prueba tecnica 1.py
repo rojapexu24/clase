036ad5e9a995156b6d361ac79584548a0d7d5d85
rojapexu24 <parauso30@gmail.com>
2024-08-02 07:25:17 -0600

    clase

---
+++ prueba tecnica 1.py
@@ -0,0 +1,7 @@
+nombre = {
+    'nombre': "colocho", 
+    'edad': "15",
+    'color': "celeste",
+    'tengochamb':True
+    }
+variable=nombre.get("nombre")
---
+++ random.py
@@ -0,0 +1,10 @@
+import random
+
+Diccionario= {
+            'clave1': 'ando',
+            'clave2': 'cachondo',
+            'clave3': 'si',
+    }
+
+variable=random.choice(list(Diccionario.values()))
+print(variable)
